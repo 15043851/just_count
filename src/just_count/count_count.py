80ad6c528d55557e8118697f69b03dba1a9d46af
15043851 <lisa.heikamp@student.uva.nl>
2024-11-11 15:34:01 +0100

    Put all into def main() and print the answer

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,7 +1,6 @@
-import square
+from just_count.square import square
 
 def main():
-    square = square.square(5)
-    return square
+    print(f"The square of 5 is {square(5)}")
 
-print(f"The square of 5 is {main()}")
+main()
