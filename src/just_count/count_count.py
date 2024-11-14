94de6961d0005f3a4550176b9b934930904e173e
15043851 <lisa.heikamp@student.uva.nl>
2024-11-14 09:46:55 +0100

    Update count_count.py

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,6 +1,10 @@
 from just_count.square import square
+import click
 
-def main():
-    print(f"The square of 5 is {square(5)}")
+@click.command()
+@click.argument("number", type=int)
+def main(number):
+    print(f"The square of {number} is {square(number)}")
 
-
+if __name__ == '__main__':
+    main()
